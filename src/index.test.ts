57e666c008f289e2aed8322d532396c7e9d8c305
Jon Oropeza <joropeza@gmail.com>
2020-04-18 11:35:13 -0700

    baseline functionality

---
+++ src/index.test.ts
@@ -0,0 +1,11 @@
+// const TimezoneEnum = require('./index.ts');
+import TimezoneEnum from './index';
+
+const doStuff = (thing: TimezoneEnum) => {
+  console.log(thing);
+};
+
+test('json data is converted to an enum', () => {
+  doStuff(TimezoneEnum['Africa/Algiers']);
+  doStuff('Africa/Algiers');
+});
