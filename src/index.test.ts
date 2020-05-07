d3f8d7bfd36bad8aaaa2a6f6e6f39e6a97ef82dc
Jon Oropeza <joropeza@gmail.com>
2020-05-06 19:53:22 -0700

    fix tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,12 @@
 // const TimezoneEnum = require('./index.ts');
 import TimezoneEnum from './index';
 
-const doStuff = (thing: TimezoneEnum) => {
-  console.log(thing);
-};
+const doStuff = (thing: string) => TimezoneEnum[thing];
 
 test('json data is converted to an enum', () => {
-  doStuff(TimezoneEnum['Africa/Algiers']);
+  expect(doStuff('Africa/Algiers').toBeTruthy());
+});
+
+test('trying to refer to an invalid enum fails', () => {
+  expect(doStuff('Not A Timezone').toThrow());
 });
