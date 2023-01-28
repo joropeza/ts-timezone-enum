cc7ddf58219bcf5e6dfd7c458cfadcd7d9ee7ebd
Jon Oropeza <joropeza@gmail.com>
2023-01-28 08:14:41 -0800

    update dependencies and fix test suite

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,12 +1,15 @@
+/* eslint-disable import/no-extraneous-dependencies */
 // const TimezoneEnum = require('./index.ts');
+import { expect, test } from '@jest/globals';
+
 import TimezoneEnum from './index';
 
-const doStuff = (thing: string) => TimezoneEnum[thing];
+const doStuff = (thing: string) => TimezoneEnum[thing as TimezoneEnum];
 
 test('json data is converted to an enum', () => {
-  expect(doStuff('Africa/Algiers').toBeTruthy());
+  expect(doStuff('Africa/Algiers')).toBeTruthy();
 });
 
 test('trying to refer to an invalid enum fails', () => {
-  expect(doStuff('Not A Timezone').toThrow());
+  expect(doStuff('Not A Timezone')).toBeFalsy();
 });
